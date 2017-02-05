2fc6a00584adcc8dc972e7ca28ee5bd9a153feaf
Stjepan Glavina <stjepang@gmail.com>
2017-02-05 14:40:43 +0100

    Make insertion go forwards rather than backwards

--- src/lib.rs
+++ src/lib.rs
@@ -61,55 +61,58 @@ impl<T> Drop for WriteOnDrop<T> {
     }
 }
 
-/// Inserts `v[0]` into pre-sorted sequence `v[1..]` so that whole `v[..]` becomes sorted.
-///
-/// This is the integral subroutine of insertion sort.
-fn insert_head<T, F>(v: &mut [T], is_less: &mut F)
+/// Sorts a slice using insertion sort, which is `O(n^2)` worst-case.
+fn insertion_sort<T, F>(v: &mut [T], is_less: &mut F)
     where F: FnMut(&T, &T) -> bool
 {
-    if v.len() >= 2 && is_less(&v[1], &v[0]) {
+    let len = v.len();
+
+    for i in 1..len {
         unsafe {
-            // There are three ways to implement insertion here:
-            //
-            // 1. Swap adjacent elements until the first one gets to its final destination.
-            //    However, this way we copy data around more than is necessary. If elements are big
-            //    structures (costly to copy), this method will be slow.
-            //
-            // 2. Iterate until the right place for the first element is found. Then shift the
-            //    elements succeeding it to make room for it and finally place it into the
-            //    remaining hole. This is a good method.
-            //
-            // 3. Copy the first element into a temporary variable. Iterate until the right place
-            //    for it is found. As we go along, copy every traversed element into the slot
-            //    preceding it. Finally, copy data from the temporary variable into the remaining
-            //    hole. This method is very good. Benchmarks demonstrated slightly better
-            //    performance than with the 2nd method.
-            //
-            // All methods were benchmarked, and the 3rd showed best results. So we chose that one.
-            let mut tmp = NoDrop { value: Some(ptr::read(&v[0])) };
+            if is_less(v.get_unchecked(i), v.get_unchecked(i - 1)) {
+                // There are three ways to implement insertion here:
+                //
+                // 1. Swap adjacent elements until the first one gets to its final destination.
+                //    However, this way we copy data around more than is necessary. If elements are
+                //    big structures (costly to copy), this method will be slow.
+                //
+                // 2. Iterate until the right place for the first element is found. Then shift the
+                //    elements succeeding it to make room for it and finally place it into the
+                //    remaining hole. This is a good method.
+                //
+                // 3. Copy the first element into a temporary variable. Iterate until the right
+                //    place for it is found. As we go along, copy every traversed element into the
+                //    slot preceding it. Finally, copy data from the temporary variable into the
+                //    remaining hole. This method is very good. Benchmarks demonstrated slightly
+                //    better performance than with the 2nd method.
+                //
+                // All methods were benchmarked, and the 3rd showed best results. So we chose that
+                // one.
+                let mut tmp = NoDrop { value: Some(ptr::read(v.get_unchecked(i))) };
 
-            // Intermediate state of the insertion process is always tracked by `hole`, which
-            // serves two purposes:
-            // 1. Protects integrity of `v` from panics in `is_less`.
-            // 2. Fills the remaining hole in `v` in the end.
-            //
-            // Panic safety:
-            //
-            // If `is_less` panics at any point during the process, `hole` will get dropped and
-            // fill the hole in `v` with `tmp`, thus ensuring that `v` still holds every object it
-            // initially held exactly once.
-            let mut hole = InsertionHole {
-                src: tmp.value.as_mut().unwrap(),
-                dest: &mut v[1],
-            };
-            ptr::copy_nonoverlapping(&v[1], &mut v[0], 1);
+                // Intermediate state of the insertion process is always tracked by `hole`, which
+                // serves two purposes:
+                // 1. Protects integrity of `v` from panics in `is_less`.
+                // 2. Fills the remaining hole in `v` in the end.
+                //
+                // Panic safety:
+                //
+                // If `is_less` panics at any point during the process, `hole` will get dropped and
+                // fill the hole in `v` with `tmp`, thus ensuring that `v` still holds every object
+                // it initially held exactly once.
+                let mut hole = InsertionHole {
+                    src: tmp.value.as_mut().unwrap(),
+                    dest: v.get_unchecked_mut(i - 1),
+                };
+                ptr::copy_nonoverlapping(v.get_unchecked(i - 1), v.get_unchecked_mut(i), 1);
 
-            for i in 2..v.len() {
-                if !is_less(&v[i], &tmp.value.as_ref().unwrap()) {
-                    break;
+                for j in (0..i-1).rev() {
+                    if !is_less(&tmp.value.as_ref().unwrap(), v.get_unchecked(j)) {
+                        break;
+                    }
+                    ptr::copy_nonoverlapping(v.get_unchecked(j), v.get_unchecked_mut(j + 1), 1);
+                    hole.dest = v.get_unchecked_mut(j);
                 }
-                ptr::copy_nonoverlapping(&v[i], &mut v[i - 1], 1);
-                hole.dest = &mut v[i];
             }
             // `hole` gets dropped and thus copies `tmp` into the remaining hole in `v`.
         }
@@ -135,18 +138,6 @@ fn insert_head<T, F>(v: &mut [T], is_less: &mut F)
     impl<T> Drop for InsertionHole<T> {
         fn drop(&mut self) {
             unsafe { ptr::copy_nonoverlapping(self.src, self.dest, 1); }
-        }
-    }
-}
-
-/// Sorts a slice using insertion sort, which is `O(n^2)` worst-case.
-fn insertion_sort<T, F>(v: &mut [T], is_less: &mut F)
-    where F: FnMut(&T, &T) -> bool
-{
-    let len = v.len();
-    if len >= 2 {
-        for i in (0..len-1).rev() {
-            insert_head(&mut v[i..], is_less);
         }
     }
 }
